44799d95f53e33c3d18c039f1988dec2dd57506c
DmitrenkoDmN <DmitrenkoDmN@gmail.com>
2018-06-09 08:18:48 +0300

    Initial commit

---
+++ my-statement-data.js
@@ -0,0 +1,507 @@
+const myStatementData = [
+    {
+        _id: '5ae17f666641d44ec7d772e1',
+        date: '2015-02-12T06:40:26',
+        type: 'Кредит',
+        amount: -3495.31,
+    },
+    {
+        _id: '5ae17f668f6e31eeccbad86b',
+        date: '2016-12-22T09:40:25',
+        type: 'Перевод',
+        amount: -56.06,
+    },
+    {
+        _id: '5ae17f66c0cbe4aba08dcf4a',
+        date: '2018-03-31T07:14:44',
+        type: 'Перевод',
+        amount: 2590,
+    },
+    {
+        _id: '5ae17f6629d2125fead49e6d',
+        date: '2015-08-05T08:41:31',
+        type: 'Перевод',
+        amount: 43388.28,
+    },
+    {
+        _id: '5ae17f66c56ae1efed72f85a',
+        date: '2016-09-26T12:05:30',
+        type: 'Перевод',
+        amount: 83769.05,
+    },
+    {
+        _id: '5ae17f66e68d29a7fc96fe9d',
+        date: '2016-02-18T08:12:25',
+        type: 'Начисление процентов',
+        amount: 23320.15,
+    },
+    {
+        _id: '5ae17f6657f62ef3b1745695',
+        date: '2015-03-21T05:59:35',
+        type: 'Магазины',
+        amount: 20111.3,
+    },
+    {
+        _id: '5ae17f66eb33ec3d20fd2fae',
+        date: '2017-01-02T07:51:58',
+        type: 'Кредит',
+        amount: 13878.56,
+    },
+    {
+        _id: '5ae17f668a5410c3583fd77a',
+        date: '2014-09-09T01:39:11',
+        type: 'Перевод',
+        amount: 76948.45,
+    },
+    {
+        _id: '5ae17f668a2f2f36398c369c',
+        date: '2016-11-09T04:02:18',
+        type: 'Кредит',
+        amount: 31158.44,
+    },
+    {
+        _id: '5ae17f6687b1f72ce2bfad3d',
+        date: '2017-01-02T05:57:23',
+        type: 'Магазины',
+        amount: 59593.8,
+    },
+    {
+        _id: '5ae17f66ae9dc1477ff6300f',
+        date: '2015-04-27T09:01:44',
+        type: 'Кредит',
+        amount: 83519.88,
+    },
+    {
+        _id: '5ae17f66db15f5a7da4c7584',
+        date: '2015-02-19T01:55:37',
+        type: 'Магазины',
+        amount: 18049.58,
+    },
+    {
+        _id: '5ae17f664665a2ebf292e752',
+        date: '2016-09-26T09:02:12',
+        type: 'Перевод',
+        amount: 51779.86,
+    },
+    {
+        _id: '5ae17f665aa43385a3d52475',
+        date: '2014-12-18T08:09:29',
+        type: 'Начисление процентов',
+        amount: 83511.73,
+    },
+    {
+        _id: '5ae17f66c1b9ed550fd60812',
+        date: '2017-02-20T05:58:47',
+        type: 'Кредит',
+        amount: 83292.78,
+    },
+    {
+        _id: '5ae17f662c7164918520db88',
+        date: '2017-05-26T07:19:42',
+        type: 'Кредит',
+        amount: -4475.79,
+    },
+    {
+        _id: '5ae17f66856871ac8af7de67',
+        date: '2017-09-02T12:37:39',
+        type: 'Начисление процентов',
+        amount: 12165.78,
+    },
+    {
+        _id: '5ae17f66b4ea99224a09d7cb',
+        date: '2017-02-08T01:15:06',
+        type: 'Кредит',
+        amount: 8388.97,
+    },
+    {
+        _id: '5ae17f66217a73e4b4924055',
+        date: '2017-09-15T12:48:26',
+        type: 'Перевод',
+        amount: 90960.39,
+    },
+    {
+        _id: '5ae17f6661b9a3528db11b9d',
+        date: '2015-04-19T01:23:55',
+        type: 'Магазины',
+        amount: 55039.33,
+    },
+    {
+        _id: '5ae17f6657beacdd4e24f726',
+        date: '2016-08-26T02:30:16',
+        type: 'Начисление процентов',
+        amount: 59616.09,
+    },
+    {
+        _id: '5ae17f66a82fe0a4980a0dc3',
+        date: '2017-03-06T11:49:01',
+        type: 'Кредит',
+        amount: 1786.82,
+    },
+    {
+        _id: '5ae17f66c649906f23644214',
+        date: '2017-03-19T01:35:43',
+        type: 'Кредит',
+        amount: 83842.55,
+    },
+    {
+        _id: '5ae17f665469da535f874774',
+        date: '2017-03-26T04:45:48',
+        type: 'Магазины',
+        amount: 14347.64,
+    },
+    {
+        _id: '5ae17f66dcb5349dea2b456d',
+        date: '2016-02-17T09:24:02',
+        type: 'Перевод',
+        amount: 24817.82,
+    },
+    {
+        _id: '5ae17f66eafbf2d367247ef7',
+        date: '2015-10-11T03:23:55',
+        type: 'Кредит',
+        amount: 97439.9,
+    },
+    {
+        _id: '5ae17f6618baa8e46746a366',
+        date: '2018-04-24T08:28:54',
+        type: 'Перевод',
+        amount: -7430.31,
+    },
+    {
+        _id: '5ae17f66c32acd0ebe4f100c',
+        date: '2015-04-18T08:20:40',
+        type: 'Начисление процентов',
+        amount: 20487.86,
+    },
+    {
+        _id: '5ae17f668920bc8ee05e5558',
+        date: '2017-04-20T09:09:45',
+        type: 'Начисление процентов',
+        amount: 41407.38,
+    },
+    {
+        _id: '5ae17f66c53131c4de422594',
+        date: '2015-11-22T12:52:39',
+        type: 'Кредит',
+        amount: 88014.82,
+    },
+    {
+        _id: '5ae17f66d617ff9149c68c55',
+        date: '2016-11-01T10:25:55',
+        type: 'Кредит',
+        amount: 35947.58,
+    },
+    {
+        _id: '5ae17f6668db5715941c8835',
+        date: '2015-09-22T06:20:16',
+        type: 'Кредит',
+        amount: -5301.11,
+    },
+    {
+        _id: '5ae17f6640c5a0e23a075d2f',
+        date: '2016-09-06T06:05:16',
+        type: 'Магазины',
+        amount: 98331.39,
+    },
+    {
+        _id: '5ae17f662a9ff83bf08f2095',
+        date: '2014-08-23T06:48:55',
+        type: 'Магазины',
+        amount: 38431.21,
+    },
+    {
+        _id: '5ae17f66c8b94bbbf09fb027',
+        date: '2016-08-22T05:34:03',
+        type: 'Начисление процентов',
+        amount: 69438.38,
+    },
+    {
+        _id: '5ae17f6668ebe845b3980d97',
+        date: '2017-10-29T02:36:58',
+        type: 'Кредит',
+        amount: 77305.27,
+    },
+    {
+        _id: '5ae17f66f6655d3e438a60b5',
+        date: '2015-02-18T08:18:38',
+        type: 'Кредит',
+        amount: 18087.04,
+    },
+    {
+        _id: '5ae17f66d4f67524947ced5d',
+        date: '2017-03-09T04:46:24',
+        type: 'Начисление процентов',
+        amount: 74078.78,
+    },
+    {
+        _id: '5ae17f66a3fb7a335450e076',
+        date: '2016-03-04T09:51:48',
+        type: 'Перевод',
+        amount: -3455.41,
+    },
+    {
+        _id: '5ae17f66eef06ea06fd9e250',
+        date: '2014-06-03T12:58:53',
+        type: 'Начисление процентов',
+        amount: 7512.29,
+    },
+    {
+        _id: '5ae17f66996d95c7420a4938',
+        date: '2016-04-18T05:06:07',
+        type: 'Магазины',
+        amount: 63408.43,
+    },
+    {
+        _id: '5ae17f664a399af51daaa9a2',
+        date: '2014-10-08T04:50:08',
+        type: 'Магазины',
+        amount: -1751.87,
+    },
+    {
+        _id: '5ae17f66d06233db53d8dee2',
+        date: '2015-03-26T01:56:05',
+        type: 'Кредит',
+        amount: 90868.74,
+    },
+    {
+        _id: '5ae17f661bca1bd742ada082',
+        date: '2016-04-02T01:04:02',
+        type: 'Магазины',
+        amount: -3064.64,
+    },
+    {
+        _id: '5ae17f66ebe658ba0b45961b',
+        date: '2014-02-02T09:43:50',
+        type: 'Начисление процентов',
+        amount: 20437.17,
+    },
+    {
+        _id: '5ae17f66bcb7b1b152555a7c',
+        date: '2014-02-14T11:47:34',
+        type: 'Кредит',
+        amount: 43544.84,
+    },
+    {
+        _id: '5ae17f661ada73ba8d30b51b',
+        date: '2014-05-05T08:48:40',
+        type: 'Начисление процентов',
+        amount: -3219.46,
+    },
+    {
+        _id: '5ae17f66c6064f4a0616dcba',
+        date: '2017-08-18T12:05:09',
+        type: 'Магазины',
+        amount: -8006.73,
+    },
+    {
+        _id: '5ae17f66e5932afad62787db',
+        date: '2016-05-31T08:29:26',
+        type: 'Перевод',
+        amount: 89428.83,
+    },
+];
+
+function findIndex(date) {
+    var temp = 0;
+    var table = document.getElementById("table1");
+    if (table.rows[0].cells[0])
+        for (var i = 0, row; row = table.rows[i]; i++) {
+            var col = row.cells[0];
+            if (!col)
+                break;
+            if (dateSort(col.innerHTML, date) == 1)
+                temp++;
+            else return temp;
+        }
+    return temp;
+}
+
+function dateSort(a, b) {
+    a = a.split('.').reverse().join('');
+    b = b.split('.').reverse().join('');
+    return a > b ? 1 : 0;
+}
+
+function findSameDate(date) {
+    var table = document.getElementById("table1");
+    if (table.rows[0].cells[0])
+        for (var i = 0, row; row = table.rows[i]; i++) {
+            var col = row.cells[0];
+            if (!col)
+                break;
+            if (col.innerHTML == date)
+                return row;
+        }
+    return -1;
+}
+
+function showOrHide(cb) {
+    cb = document.getElementById(cb);
+    myTable = document.getElementById("table1");
+    var i;
+    var col = cb.id.substring(2) - 1;
+    if (cb.checked) {
+        myTable.rows[0].cells[col].setAttribute("class", "")
+    }
+    else myTable.rows[0].cells[col].setAttribute("class", "invisibleClass")
+    for (i = 1; i < myTable.rows.length; i++) {
+        if (cb.checked) {
+            if (col == 4)
+                myTable.rows[i].cells[col].setAttribute("class", "redClass")
+            else if (col == 3)
+                myTable.rows[i].cells[col].setAttribute("class", "greenClass")
+            else
+                myTable.rows[i].cells[col].setAttribute("class", "")
+        }
+        else
+            myTable.rows[i].cells[col].setAttribute("class", "invisibleClass")
+    }
+    var temp = 0;
+    for (i = 1; i < 6; i++) {
+        if (document.getElementById("cb" + i).checked)
+            temp++;
+    }
+    if (temp == 1) {
+        for (i = 1; i < 6; i++) {
+            if (document.getElementById("cb" + i).checked)
+                document.getElementById("cb" + i).disabled = true;
+        }
+    }
+    else {
+        for (i = 1; i < 6; i++) {
+            document.getElementById("cb" + i).disabled = false;
+        }
+    }
+}
+
+function noGroup() {
+    var tbody = document.getElementById("table1").getElementsByTagName("tbody")[0];
+    var table = document.getElementById("table1");
+    var row = table.insertRow(0);
+    row.style.backgroundColor='#d3d3d3';
+    var cell = row.insertCell(0);
+    cell.innerHTML = "Дата";
+    cell = row.insertCell(1);
+    cell.innerHTML = "Время";
+    cell = row.insertCell(2);
+    cell.innerHTML = "Тип";
+    cell = row.insertCell(3);
+    cell.innerHTML = "Приход";
+    cell = row.insertCell(4);
+    cell.innerHTML = "Расход";
+    for (const elem of myStatementData) {
+        var date = new Date(elem.date);
+        date = date.toLocaleDateString('ru-RU');
+        row = table.insertRow(findIndex(date));
+        var cell1 = row.insertCell(0);
+        cell1.innerHTML = date;
+        var cell2 = row.insertCell(1);
+        cell2.innerHTML = time;
+        var cell3 = row.insertCell(2);
+        cell3.innerHTML = elem.type;
+        var cell4 = row.insertCell(3);
+        var cell5 = row.insertCell(4);
+        var time = elem.date.substring(11);
+        var temp = elem.amount.toLocaleString;
+        if (elem.amount > 0) {
+            cell4.innerHTML = elem.amount;
+            cell5.innerHTML = "";
+        }
+        else {
+            cell4.innerHTML = "";
+            cell5.innerHTML = Math.abs(elem.amount);
+        }
+        cell4.className = "greenClass";
+        cell5.className = "redClass";
+    }
+    for (i = 1; i < table.rows.length; i++) {
+        if (table.rows[i].cells[4].innerHTML)
+            table.rows[i].cells[4].innerHTML =
+                Number(table.rows[i].cells[4].innerHTML).toLocaleString('ru-RU', {style: 'currency', currency: 'RUB'});
+        if (table.rows[i].cells[3].innerHTML)
+            table.rows[i].cells[3].innerHTML =
+                Number(table.rows[i].cells[3].innerHTML).toLocaleString('ru-RU', {style: 'currency', currency: 'RUB'});
+    }
+    for (i = 1; i < 6; i++) {
+        document.getElementById("cb" + i).disabled = false;
+        showOrHide("cb"+i);
+    }
+}
+
+function groupByDay() {
+    var tbody = document.getElementById("table1").getElementsByTagName("tbody")[0];
+    var table = document.getElementById("table1");
+    var row = table.insertRow(0);
+
+    var cell = row.insertCell(0);
+    cell.innerHTML = "Дата";
+    cell = row.insertCell(1);
+    cell.innerHTML = "Время";
+    cell.className = "invisibleClass";
+    cell = row.insertCell(2);
+    cell.innerHTML = "Тип";
+    cell.className = "invisibleClass";
+    cell = row.insertCell(3);
+    cell.innerHTML = "колонка";
+    cell.className = "invisibleClass";
+    cell = row.insertCell(4);
+    cell.innerHTML = "Изменение счёта";
+    row.style.backgroundColor='#d3d3d3';
+    for (const elem of myStatementData) {
+        var date = new Date(elem.date);
+        date = date.toLocaleDateString('ru-RU');
+        if (findSameDate(date) == -1) {
+            var row = table.insertRow(findIndex(date));
+            var cell1 = row.insertCell(0);
+            cell1.innerHTML = date;
+            var cell2 = row.insertCell(1);
+            var cell3 = row.insertCell(2);
+            var cell4 = row.insertCell(3);
+            var cell5 = row.insertCell(4);
+            cell5.innerHTML = elem.amount;
+            cell2.className = "invisibleClass";
+            cell3.className = "invisibleClass";
+            cell4.className = "invisibleClass";
+            if (cell5.innerHTML < 0)
+                cell5.className = "redClass";
+            else if (cell5.innerHTML > 0)
+                cell5.className = "greenClass";
+            else
+                cell5.className = "visibleClass"
+        }
+        else {
+            var dailyTransactions = findSameDate(date);
+            dailyTransactions.cells[4].innerHTML = Number(dailyTransactions.cells[4].innerHTML) + elem.amount;
+            if (dailyTransactions.cells[4].innerHTML < 0)
+                dailyTransactions.cells[4].className = "redClass";
+            else if (dailyTransactions.cells[4].innerHTML > 0)
+                dailyTransactions.cells[4].className = "greenClass";
+            else
+                dailyTransactions.cells[4].className = "visibleClass"
+        }
+    }
+    for (i = 1; i < table.rows.length; i++) {
+        table.rows[i].cells[4].innerHTML =
+            Number(table.rows[i].cells[4].innerHTML).toLocaleString('ru-RU', {style: 'currency', currency: 'RUB'});
+    }
+    for (i = 1; i < 6; i++)
+        document.getElementById("cb" + i).disabled = true;
+
+}
+
+function groupBy(option) {
+    myTable = document.getElementById("table1");
+    var temp = myTable.rows.length;
+    for (i = 0; i < temp; i++) {
+        myTable.deleteRow(0);
+    }
+    if (option == "noGroup") {
+        noGroup();
+    }
+    else if (option == "groupByDays") {
+        groupByDay();
+    }
+}
+
+
+noGroup();
